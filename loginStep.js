c4703cbae87ec88d4c9fa277f751809d83b950db
mbgshk <mbgshk@gmail.com>
2022-07-15 08:27:29 +0700

    Define Steps

--- loginStep.js
+++ loginStep.js
@@ -1,1 +1,13 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+Given ('I open login page', () => {
+    // TODO
+})
+
+When ('I submit login', () => {
+    // TODO
+})
+
+Then ('I should see homepage', () => {
+    // TODO
+})
