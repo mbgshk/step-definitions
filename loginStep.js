abf6ab585c8c2967dbfd3a582f2742accc85291d
mbgshk <mbgshk@gmail.com>
2022-07-16 09:39:53 +0700

    Final Code

--- loginStep.js
+++ loginStep.js
@@ -1,15 +1,16 @@
 /// <reference types="cypress" />
 
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import { LoginPage } from "./loginPage";
 
 Given ('I open login page', () => {
-    cy.visit('http://zero.webappsecurity.com/login.html')
+    LoginPage.visit()
 })
 
 When ('I submit login', () => {
-    cy.get('#user_login').type('username')
-    cy.get('#user_password').type('password')
-    cy.get('input[name="submit"]').click()
+    LoginPage.fillUsername('username')
+    LoginPage.fillPassword('password')
+    LoginPage.signIn()
 })
 
 Then ('I should see homepage', () => {
